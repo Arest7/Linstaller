04e8235aab4a0a55e6bd33e61b9a125d8e3c7800
Arest7 <73108998+Arest7@users.noreply.github.com>
2020-10-19 15:41:00 +0500

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,24 @@
+apt update && apt upgrade
+
+apt install figlet
+
+возможность установить туалет
+
+Удачная установка Cowsay
+Чисто
+
+фиглет Бисмиллах
+
+спать 3
+
+подходящая установка рубина
+
+gem install lolcat
+спать 2
+туалет большой Alhamdulillah | лолкот
+
+эхо иш битди ишлатиш:
+echo lolat установлен для запуска:
+эхо
+эхо  - туалет -f большой хакер | Lolcat '
+echo  ' cowsay -f eyes Хакер | Lolcat '
